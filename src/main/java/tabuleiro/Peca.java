d4fa5ba810a6f982297c94c28039be3a6fd12b98
Mateus <mateus.eneas98@gmail.com>
2023-05-23 12:01:44 -0300

    Iniciando a implementação do tabuleiro e peças

---
+++ src/main/java/tabuleiro/Peca.java
@@ -0,0 +1,16 @@
+package tabuleiro;
+
+public class Peca {
+
+    protected Posicao posicao;
+    private Tabuleiro tabuleiro;
+
+    public Peca(Tabuleiro tabuleiro) {
+        this.tabuleiro = tabuleiro;
+    }
+
+    public Tabuleiro getTabuleiro() {
+        return tabuleiro;
+    }
+
+}
